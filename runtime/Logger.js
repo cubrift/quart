338083954515fdcab3e283dcdb7f54af4eace7a3
Cubrift <david@skillmanmedia.com>
2026-08-06 13:31:49 +0100

    Make pino pretty regardless

--- runtime/Logger.js
+++ runtime/Logger.js
@@ -1,11 +1,9 @@
 const pino = require('pino');
 const pretty = require('pino-pretty');
 
-const isDev = process.env.NODE_ENV !== 'production';
-
 const logger = pino(
   { level: process.env.LOG_LEVEL || 'info' },
-  isDev ? pretty({ colorize: true }) : undefined
+  pretty({ colorize: true })
 );
 
 function handleRejection(reason, promise) {
